18c56313a8e3d78ed6fa0296ad31e7e758c9bb05
youngtoo <ianktoo@gmail.com>
2021-02-09 20:22:38 +0300

    Created the project.

---
+++ src/test/java/com/iantoo/demo/SimpleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iantoo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
